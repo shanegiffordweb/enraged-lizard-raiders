fbb1d8d35304f49aba498206f70b3e23ac58bd6b
CNarvaiz <cj.narvaiz@gmail.com>
2020-02-26 22:09:13 -0600

    Lizard now moves left and right. Gravity works. Code is only mildly stolen.

---
+++ Assets/LizardController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LizardController : MonoBehaviour
+{
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;
+    private Vector3 m_Velocity = Vector3.zero;
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Move(float move)
+    {// Move the character by finding the target velocity
+			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}}
+    private void Flip()
+     {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+     }
+
+}
---
+++ Assets/LizardMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LizardMovement : MonoBehaviour
+{
+    public LizardController controller;
+    float horizontalMove = 0f;
+    public float speed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalMove =  Input.GetAxisRaw("Horizontal") * speed;
+        
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime);
+    }
+}
